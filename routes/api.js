e50163d6a91136dbc72b052c0f2d89b1782674a6
jkalmar <janko.kalmar@gmail.com>
2017-06-17 23:41:57 +0200

    Added handling off post

--- routes/api.js
+++ routes/api.js
@@ -1,29 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
-const data = [
+var data = [
     {
         "id" : "1",
-        "name" : "vlado",
-        "location" : "40 50"
+        "name" : "test1",
+        "location" : "40 50",
+        "history" : [ "l1", "l2", "l3" ]
     },
     {
         "id" : "2",
-        "name" : "jano",
-        "location" : "100 500"
+        "name" : "test2",
+        "location" : "100 500",
+        "history" : []
     },
     {
         "id" : "3",
-        "name" : "veronika",
-        "location" : "1 5"
+        "name" : "test3",
+        "location" : "1 5",
+        "history" : []
     },
     {
         "id" : "5",
-        "name" : "test2",
-        "location" : "4654654 1212"
+        "name" : "test4",
+        "location" : "4654654 1212",
+        "history" : []
     }
 ]
 
+
+let getTaxi = function( id )
+{
+    for( let i = 0; i < data.length; i++ )
+    {
+        if( data[ i ].id === id )
+        {
+            return data[ i ];
+        }
+    }
+}
 
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
@@ -34,23 +49,37 @@ router.get('/users', function(req, res, next) {
 });
 
 router.get('/users/:id', function(req, res, next) {
-    let i = 0;
-
     console.log( req.params.id );
 
-    for( i; i < data.length; i++ )
+    let d = getTaxi( req.params.id );
+
+    if( d )
     {
-        if( data[i].id == req.params.id )
-        {
-            break;
-        }
+        res.render('taxi', { user : d } );
     }
-
-    console.log( i );
-    console.log( data[i] );
-
-    res.render('taxi', { user : data[i] })
+    else
+    {
+        res.render( 'taxi', { user : { "id" : "Nan", "name" : "Default", "location" : "Default", "history" : [] } } );
+    }
 });
+
+router.post( '/api/add', function( req, res )
+{
+    data.push( req.body ); 
+
+    console.log( req.body );
+
+    res.sendStatus( 200 );
+});
+
+router.post( '/api/gps/:id', function( req, res )
+{
+    let driver = getTaxi( req.params.id );
+
+    driver.history.push( req.body.location );
+
+    res.sendStatus( 200 );
+} )
 
 
 
