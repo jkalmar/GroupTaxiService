c463be9e0edcb890d041c5f938a2191f33c5d3cf
jkalmar <janko.kalmar@gmail.com>
2017-07-06 23:52:11 +0200

    Created API stubs and commented intended behaviuour

--- routes/api.js
+++ routes/api.js
@@ -12,6 +12,100 @@ router.get('/', function(req, res, next) {
 router.get('/users', function(req, res, next) {
     res.render('taxis', { users : data })
 });
+
+/**
+ * Updates driver location
+ * Location is in lang, long GPS coordinates
+ */
+router.post('/driver/:id', ( req, res, next ) => {
+
+} );
+
+/**
+ * Send message to user
+ */
+router.post('/communication', ( req, res, next ) => {
+
+} );
+
+/**
+ * Get all pending messages from chat
+ */
+router.get('/comunication', ( req, res, next ) => {
+
+});
+
+/**
+ * Post new order from user to system
+ */
+router.post('/order',( req, res, next ) => {
+
+});
+
+/**
+ * Get new orders from system to taxi
+ * This considers also driver total orders
+ */
+router.get('/order/:driverId', ( req, res, next ) => {
+
+});
+
+/**
+ * Take specific order
+ * This will send confirmating respons to driver in case there are more
+ * drivers taking this order simultanuosly
+ */
+router.post('/take/:id', ( req, res, next ) => {
+
+});
+
+
+/**
+ * Register user to database
+ * Creates new user in db and afterward allow this user to rate and make orders for others
+ */
+router.post('/register', ( req, res, next ) => {
+
+});
+
+/**
+ * Rate driver
+ * Registered user can rate driver
+ */
+router.post('/rate/:driverId', ( req, res, next ) => {
+
+});
+
+/**
+ * Blacklist a user
+ * Blaclisted user can not make new order as this user was doing something bad in the past
+ * In the post body there has to be info about who to blacklist, newer blacklist user by his
+ * registered id, but rather phone id or name or phone number so the user can be detected
+ * even from different location/registration
+ */
+router.post('/blacklist', ( req, res, next ) => {
+
+});
+
+/**
+ * Get all blacklisted users
+ * Send all blacklisted users to driver
+ */
+router.get('/blacklist', ( req, res, next ) => {
+
+});
+
+
+/**
+ * Deletes blacklisted user from blacklist
+ * User can now make orders, json send to server is generater from GET /blacklist and from those
+ * information about user so he/she can be removed from DB
+ */
+router.delete('/blacklist', ( req, res, next ) => {
+
+});
+
+
 /*
 router.get('/users/:id', function(req, res, next) {
     console.log( req.params.id );
