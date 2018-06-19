c7b2aa80a129466d9faec3e61d0785963dfd6d45
jkalmar <janko.kalmar@gmail.com>
2018-06-19 19:27:04 +0200

    Added users module stub

--- models/users.js
+++ models/users.js
@@ -1,1 +1,26 @@
 const db = require('./database');
+
+const sqlGetAllUsers = "SELECT id, username, phone FROM customers"
+
+
+
+
+
+
+
+
+
+
+
+/**
+ *
+ * @param {Express.Router} app
+ */
+function init( app ) {
+
+}
+
+
+module.exports = {
+    init
+}
