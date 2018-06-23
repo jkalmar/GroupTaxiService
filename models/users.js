d06d3dc2674b09ea07841f591831037a75639f3e
jkalmar <janko.kalmar@gmail.com>
2018-06-23 13:44:22 +0200

    Users sql statements

--- models/users.js
+++ models/users.js
@@ -1,10 +1,15 @@
 const db = require('./database');
 
-const sqlGetAllUsers = "SELECT id, username, phone FROM customers"
+const sqlGetAllUsers = "SELECT id, nick, phone, email, address FROM customers"
+const sqlInserNewUser = "INSERT INTO customers (`phone`) VALUES (?)"
+const sqlUpdateNick = "UPDATE LOW_PRIORITY `customers` SET `nick`=? WHERE `id`=? limit 1"
+const sqlUpdateEmail = "UPDATE LOW_PRIORITY `customers` SET `email`=? WHERE `id`=? limit 1"
+const sqlUpdateAddress = "UPDATE LOW_PRIORITY `customers` SET `address`=? WHERE `id`=? limit 1"
+
+function newUser( phone ){
 
 
-
-
+}
 
 
 
