d2b63773d7e16dfd72452ab070a2846ec9b35ae1
jkalmar <janko.kalmar@gmail.com>
2019-03-23 22:06:24 +0100

    reformat

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var passport   = require('passport')
+var passport = require('passport')
 var bodyParser = require('body-parser');
 
 const db = require('./models/database');
 
-const api_routes = require( './routes/api' )
-const sessionParser = require( "./config/session" )
+const api_routes = require('./routes/api')
+const sessionParser = require("./config/session")
 
 var app = express();
 
 // disable etag generating for every route
 // it should not be used for API that change with every request either way
-app.set( 'etag', false );
+app.set('etag', false);
 
 // require the config json from filesystem
 // save all configurable parameters to that file
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 // add session middleware
 // this will add sessionId to every response and retieve it from every request
 // the session is stored into cookie and TODO: mariadb
-app.use( sessionParser );
+app.use(sessionParser);
 
 // add passport middleware
 // responsible of handling the user authorization to the system
@@ -41,10 +41,10 @@ app.use( sessionParser );
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
-api_routes( app );
+api_routes(app);
 
 const auth = require('./controllers/auth');
-auth.setUp( app );
+auth.setUp(app);
 
 
 // handling of static files
@@ -54,14 +54,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
 
   res.locals.message = err.message;
--- routes/api.js
+++ routes/api.js
@@ -1,61 +1,60 @@
 var express = require('express');
 var router = express.Router();
 const debug = require('debug')('backend:api');
-var passport   = require('passport');
 const auth = require('../controllers/auth');
 const issues = require('../models/issues');
-const views = require( "../models/views" );
+const views = require("../models/views");
 
 const taxi_drivers = require('../models/driver');
 
 const trouble = require('../models/trouble');
-const orders = require( "../models/orders" )
-const blacklist = require( "../models/blacklist" )
+const orders = require("../models/orders")
+const blacklist = require("../models/blacklist")
 const users = require("../controllers/users")
-const ordersApi = require( "./orders" )
+const ordersApi = require("./orders")
 const driverApi = require("./driver")
 
-users( router )
-ordersApi( router )
-driverApi( router )
+users(router)
+ordersApi(router)
+driverApi(router)
 
-router.get('/v1', function(req, res, next) {
-    views.incView( "index" ).then( value => {
-        res.render( "index" );
-    } ).catch( value => {
-        res.sendStatus( 500 );
-    } )
+router.get('/v1', function (req, res, next) {
+    views.incView("index").then(value => {
+        res.render("index");
+    }).catch(value => {
+        res.sendStatus(500);
+    })
 });
 
-router.get('/drivers',  function(req, res, next) {
-    taxi_drivers.getTaxis().then( value => {
-        res.json({ users : value });
-    } ).catch( value => {
-        res.sendStatus( 500 );
-    } )
+router.get('/drivers', function (req, res, next) {
+    taxi_drivers.getTaxis().then(value => {
+        res.json({ users: value });
+    }).catch(value => {
+        res.sendStatus(500);
+    })
 });
 
 /**
  * Send message to user
  */
-router.post('/communication', ( req, res, next ) => {
-    console.log( req.body )
-    res.sendStatus( 200 );
-} );
+router.post('/communication', (req, res, next) => {
+    console.log(req.body)
+    res.sendStatus(200);
+});
 
 /**
  * Get all pending messages from chat
  */
-router.get('/comunication', ( req, res, next ) => {
-    console.log( req.body )
-    res.send( "comunication" );
+router.get('/comunication', (req, res, next) => {
+    console.log(req.body)
+    res.send("comunication");
 });
 
 /**
  * Post new order from user to system
  */
-router.post('/order',( req, res, next ) => {
-    orders.createNewOrder( res, req.body );
+router.post('/order', (req, res, next) => {
+    orders.createNewOrder(res, req.body);
 });
 
 router.get("/order/:id/cancel", orders.cancelOrder);
@@ -66,15 +65,15 @@ router.get("/order/:id/check", orders.checkOrder)
  * Get new orders from system to taxi
  * This considers also driver total orders
  */
-router.get('/order/:driverId', ( req, res, next ) => {
+router.get('/order/:driverId', (req, res, next) => {
 
 });
 
 /**
  * Get all orders from system that belongs to a user
  */
-router.get('/order', ( req, res, next ) => {
-    res.send( "Orders" );
+router.get('/order', (req, res, next) => {
+    res.send("Orders");
 });
 
 /**
@@ -82,7 +81,7 @@ router.get('/order', ( req, res, next ) => {
  * This will send confirmating respons to driver in case there are more
  * drivers taking this order simultanuosly
  */
-router.post('/take/:id', ( req, res, next ) => {
+router.post('/take/:id', (req, res, next) => {
 
 });
 
@@ -90,7 +89,7 @@ router.post('/take/:id', ( req, res, next ) => {
  * Rate driver
  * Registered user can rate driver
  */
-router.post('/rate/:driverId', ( req, res, next ) => {
+router.post('/rate/:driverId', (req, res, next) => {
 
 });
 
@@ -101,7 +100,7 @@ router.post('/rate/:driverId', ( req, res, next ) => {
  * registered id, but rather phone id or name or phone number so the user can be detected
  * even from different location/registration
  */
-router.post('/blacklist', auth.isLoggedIn, function( req, res, next ){
+router.post('/blacklist', auth.isLoggedIn, function (req, res, next) {
 
 });
 
@@ -109,12 +108,12 @@ router.post('/blacklist', auth.isLoggedIn, function( req, res, next ){
  * Get all blacklisted users
  * Send all blacklisted users to driver
  */
-router.get('/blacklist', auth.isLoggedIn, ( req, res, next ) => {
-    blacklist.getAll().then( value => {
-        res.json( { data : value } );
-    } ).catch( err => {
+router.get('/blacklist', auth.isLoggedIn, (req, res, next) => {
+    blacklist.getAll().then(value => {
+        res.json({ data: value });
+    }).catch(err => {
         res.sendStatus(500);
-    } )
+    })
 });
 
 /**
@@ -122,29 +121,29 @@ router.get('/blacklist', auth.isLoggedIn, ( req, res, next ) => {
  * User can now make orders, json send to server is generater from GET /blacklist and from those
  * information about user so he/she can be removed from DB
  */
-router.delete('/blacklist/:id',auth.isLoggedIn, function( req, res, next ) {
-    blacklist.deleteItem( req.params.id ).then( value => {
+router.delete('/blacklist/:id', auth.isLoggedIn, function (req, res, next) {
+    blacklist.deleteItem(req.params.id).then(value => {
         res.sendStatus(200);
-    } ).catch( err => {
+    }).catch(err => {
         res.sendStatus(500);
-    } )
+    })
 });
 
 /**
  * Get all taxis that are in trouble
  */
-router.get( '/trouble', auth.isLoggedIn, function(req, res, next ) {
-    trouble.getAll( req, res, next );
-} )
+router.get('/trouble', auth.isLoggedIn, function (req, res, next) {
+    trouble.getAll(req, res, next);
+})
 
 /**
  * Post that handle a trouble
  * Tapping panic button will send post here to indicate that a driver has
  * a problem
  */
-router.post( '/trouble', auth.isLoggedIn, function( req, res, next ) {
+router.post('/trouble', auth.isLoggedIn, function (req, res, next) {
     trouble.addNew(req, res, next);
-} )
+})
 
 /**
  * Delete that handle a trouble
@@ -152,69 +151,64 @@ router.post( '/trouble', auth.isLoggedIn, function( req, res, next ) {
  * a problem, but after the driver is safe he will send a delete several time
  * to indicate that he is ok
  */
-router.delete( '/trouble', auth.isLoggedIn, function( req, res, next ) {
+router.delete('/trouble', auth.isLoggedIn, function (req, res, next) {
     trouble.deleteDriver(req, res, next);
-} )
+})
 
-router.post( "/issue", function( req, res, next )
-{
-    debug( "got issue: " + JSON.stringify( req.body ) );
+router.post("/issue", function (req, res, next) {
+    debug("got issue: " + JSON.stringify(req.body));
 
-    issues.insertIssue( req.body ).then( value => {
-        res.sendStatus( 200 );
-    } ).catch( err => {
-        debug( "Sending 500" );
-        debug( err);
-        res.sendStatus( 500 );
-    } )
-} )
+    issues.insertIssue(req.body).then(value => {
+        res.sendStatus(200);
+    }).catch(err => {
+        debug("Sending 500");
+        debug(err);
+        res.sendStatus(500);
+    })
+})
 
-router.get( "/issues", function( req, res, next )
-{
-    issues.getAll().then( value => {
-        debug( value );
-        res.render('issues', { "issues" : value });
-    } ).catch( err => {
-        debug( err );
-        res.sendStatus( 500 );
-    } )
-} )
+router.get("/issues", function (req, res, next) {
+    issues.getAll().then(value => {
+        debug(value);
+        res.render('issues', { "issues": value });
+    }).catch(err => {
+        debug(err);
+        res.sendStatus(500);
+    })
+})
 
-router.get( "/broken", function( req, res, next )
-{
-    taxi_drivers.getBrokenConnections().then( value => {
-        res.render( "broken", { "data" : value } );
-    } ).catch( err => {
-        debug( err )
-        res.sendStatus( 500 );
-    } )
-} )
+router.get("/broken", function (req, res, next) {
+    taxi_drivers.getBrokenConnections().then(value => {
+        res.render("broken", { "data": value });
+    }).catch(err => {
+        debug(err)
+        res.sendStatus(500);
+    })
+})
 
 /**
  * Handles the new phone number
  * Checks database and generate new validation code
  */
-router.post("/auth/number", function( req, res, next )
-{
+router.post("/auth/number", function (req, res, next) {
     debug("new auth number: " + req.body.phoneNum)
-    setTimeout( () => {
+    setTimeout(() => {
         res.sendStatus(200);
-    }, 2000 )
+    }, 2000)
 
 })
 
 /**
  * Handles validation code on phone number
  */
-router.post("/auth/code", function(req, res, next)
-{
+router.post("/auth/code", function (req, res, next) {
     debug("Validation code: " + req.body.valCode)
 
-    setTimeout( () => {
+    setTimeout(() => {
         res.sendStatus(200);
-    }, 2000 )
+    }, 2000)
 })
 
-module.exports = ( app ) => {
-    app.use( "/", router );
+module.exports = (app) => {
+    app.use("/", router);
 };
